4c398d19eb121624aa7fe967606c7407fd41607f
Kirtana Madiraju <kmadiraju19@gmail.com>
2020-04-04 22:26:18 -0400

    initial

---
+++ script.js
@@ -0,0 +1,24 @@
+var questionContainerEl = document.getElementById("question-container");
+var questionEl = document.getElementById("question");
+var answerBtn = document.getElementById("answer-btn");
+var startBtn = document.getElementById("start-btn");
+var timeEl = document.getElementById("timer");
+var timeLeft = 60;
+var score = 0;
+
+startBtn.addEventListener('click', startQuiz);
+startBtn.addEventListener('click', setTimer);
+
+function setTimer(){
+    
+}
+
+
+
+
+
+var questions = [
+    {
+
+    }
+]
